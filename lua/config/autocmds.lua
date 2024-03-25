e8bc227285d991e54b2174a946b2d5df2d527c2d
Lopez Tello Villafuerte <luis.david.lopez@nordicsemi.no>
2024-03-25 15:34:28 +0100

    Update and autocommands
    - General update
    - Added autocommands to handle DITA and register file workflow files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,28 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.r",
+  command = "set filetype=xml",
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.autodita",
+  command = "set filetype=xml",
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.ditaval",
+  command = "set filetype=xml",
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.dita",
+  command = "set filetype=xml",
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.ditamap",
+  command = "set filetype=xml",
+})
