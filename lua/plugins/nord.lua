6790d99643da8a7bb75bff0e63cc3797bb40f083
lopeztel <lopeztel.gmail.com>
2025-10-23 21:35:43 +0200

    Nord theme update
    - Testing removing semantic tokens provider

--- lua/plugins/nord.lua
+++ lua/plugins/nord.lua
@@ -2,12 +2,12 @@
 
 --TODO: Check if this needs to be removed (ONLY APPLIES TO NORD)
 -- Disable LSP semantic tokens by default to fix issue with theme colors https://github.com/shaunsingh/nord.nvim/issues/143
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    client.server_capabilities.semanticTokensProvider = nil
-  end,
-})
+-- vim.api.nvim_create_autocmd("LspAttach", {
+--   callback = function(args)
+--     local client = vim.lsp.get_client_by_id(args.data.client_id)
+--     client.server_capabilities.semanticTokensProvider = nil
+--   end,
+-- })
 
 vim.g.nord_borders = true
 vim.g.nord_contrast = true
